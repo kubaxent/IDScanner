da270f8389e9d7a8281c7df37ced47476325c954
Dominik Rolewski <drolewski@gmail.com>
2019-11-19 13:31:30 +0100

    Modify if condition.

--- processText.py
+++ processText.py
@@ -53,7 +53,7 @@ def found_city(text):
         if number_found:
             city_result += c
         else:
-            if c == '0' or c == '1' or c == '2' or c == '3' or c == '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9':
+            if c.isdigit():
                 city_result += c
                 number_found = True
     return city_result
